e845890664a833c8881a0959627463b5f19d2300
venukaushik <venukaushik31@gmail.com>
2023-02-03 00:29:33 +0530

    .
    .

---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom';
+
+export default function Navbar(props) {
+
+
+    return (
+        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">{props.Navbar}</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="14" fill="currentColor" className="bi bi-house" viewBox="0 0 16 16">
+                                <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z" />
+                            </svg>Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">{props.Title}</Link>
+                        </li>
+                    </ul>
+
+                    <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+                        <input className="form-check-input" onClick={props.toggleMode} style={props.style} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.modeText}</label>
+                    </div>
+
+                    <form className="d-flex mx-3" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-danger" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav >
+    )
+}
+
+Navbar.propTypes = {
+    Navbar: PropTypes.string,
+    Title: PropTypes.string
+}
---
+++ src/components/TextForm.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText)
+        props.showAlert("Text converted to UPPERCASE", "success")
+    }
+
+    const handleLoClick = () => {
+        let newText2 = text.toLowerCase();
+        setText(newText2)
+        props.showAlert("Text converted to lowercase", "success")
+
+    }
+
+    const handleClearClick = () => {
+        let newClearText = "";
+        setText(newClearText)
+        props.showAlert("Text Cleared", "success")
+
+    }
+
+    const handleCopy = () => {
+        var text = document.getElementById('myBox')
+        text.select();
+        navigator.clipboard.writeText(text.value)
+        props.showAlert("Text copied to Clipboard", "success")
+
+    }
+
+    const speak = () => {
+        let msg = new SpeechSynthesisUtterance();
+        msg.text = text;
+        window.speechSynthesis.speak(msg);
+    }
+
+    const handleOnChange = (event) => {
+        setText(event.target.value)
+    }
+    const [text, setText] = useState("")
+    return (
+        <>
+            <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+                <h1>{props.heading}</h1>
+                <div className="mb-3 my-4">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? '#494747' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} id="myBox" rows="10"></textarea>
+                </div>
+                <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert To UPPERCASE</button>
+                <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert To lowercase</button>
+                <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
+                <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+                <button type="submit" onClick={speak} className="btn btn-warning mx-2 my-2">Speak</button>
+            </div>
+            <div className="container my-4" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+                <h2>Text Summary</h2>
+                <p>{text.split(" ").length} Words & {text.length} Characters</p>
+                <p>Time taken to read Text</p>
+                <p>{0.008 * text.split(" ").length}</p>
+                <h2>Preview</h2>
+                <p>{text.length > 0 ? text : "Enter Your Text Above To Preview It Here"}</p>
+            </div>
+        </>
+    )
+}

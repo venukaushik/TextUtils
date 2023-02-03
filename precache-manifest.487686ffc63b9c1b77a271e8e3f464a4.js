e6c6d0a3b48320975fafa5fe1d6d0a41a0e0b975
venukaushik <venukaushik31@gmail.com>
2023-02-03 13:52:31 +0530

    Updates

---
+++ precache-manifest.487686ffc63b9c1b77a271e8e3f464a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6e7464f049c9e875c1a",
+    "url": "/TextUtils/static/css/main.b3d78c5e.chunk.css"
+  },
+  {
+    "revision": "a6e7464f049c9e875c1a",
+    "url": "/TextUtils/static/js/main.a6e7464f.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/TextUtils/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "3c016175441de394fef9",
+    "url": "/TextUtils/static/js/2.3c016175.chunk.js"
+  },
+  {
+    "revision": "e61b163c9eeaceb92b78",
+    "url": "/TextUtils/static/js/runtime~main.e61b163c.js"
+  },
+  {
+    "revision": "c895df5cc95d14aee83b83b6364fcc99",
+    "url": "/TextUtils/index.html"
+  }
+];
